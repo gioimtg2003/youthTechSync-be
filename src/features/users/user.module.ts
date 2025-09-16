3ab431f2046fe871b8e6212f7ac3d27ddcc1596c
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2025-09-16 23:21:41 +0700

    feat(ui): get teams by user

--- src/features/users/user.module.ts
+++ src/features/users/user.module.ts
@@ -1,12 +1,14 @@
 import { CryptoModule } from '@features/crypto';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AbilityModule } from 'src/ability';
 import { User } from './entities/user.entity';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [CryptoModule, TypeOrmModule.forFeature([User])],
-  controllers: [],
+  imports: [CryptoModule, TypeOrmModule.forFeature([User]), AbilityModule],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
--- src/features/users/user.service.ts
+++ src/features/users/user.service.ts
@@ -1,4 +1,4 @@
-import { UserError } from '@constants';
+import { DATABASE_TABLES, UserError } from '@constants';
 import { CryptoService } from '@features/crypto';
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -61,5 +61,34 @@ export class UserService {
     this.logger.log(`Created user ${saved.id}`);
 
     return saved;
+  }
+
+  async getTeams(id: number) {
+    this.logger.log(`Getting teams for user ${id}`);
+    const teams = await this.userRepository
+      .createQueryBuilder(DATABASE_TABLES.USERS)
+      .leftJoinAndSelect(
+        `${DATABASE_TABLES.USERS}.${DATABASE_TABLES.TEAMS}`,
+        DATABASE_TABLES.TEAMS,
+      )
+      .select([
+        `${DATABASE_TABLES.TEAMS}.id`,
+        `${DATABASE_TABLES.TEAMS}.name`,
+        `${DATABASE_TABLES.TEAMS}.alias`,
+        `${DATABASE_TABLES.TEAMS}.logoUrl`,
+        `${DATABASE_TABLES.TEAMS}.settings`,
+      ])
+      .where(`${DATABASE_TABLES.USERS}.id = :id`, { id: id })
+      .getRawMany();
+
+    this.logger.log(`Found ${teams?.length} teams for user ${id}`);
+
+    return (teams ?? [])?.map((team) => ({
+      id: team?.teams_id,
+      name: team?.teams_name,
+      alias: team?.teams_alias,
+      logoUrl: team?.teams_logoUrl,
+      settings: team?.teams_settings,
+    }));
   }
 }
