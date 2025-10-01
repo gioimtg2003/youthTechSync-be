14873b409058ca2cd7a0f25f3341d721f3cff7fd
Nguyen Cong Gioi <conggioi.pro264@gmail.com>
2025-10-01 21:45:09 +0700

    ✨ feat(user): get current session

--- src/features/users/user.controller.ts
+++ src/features/users/user.controller.ts
@@ -1,10 +1,25 @@
-import { VERSIONING_API } from '@constants';
-import { Controller } from '@nestjs/common';
+import { SYSTEM_RESOURCE, VERSIONING_API } from '@constants';
+import { CurrentUser } from '@decorators';
+import { UserAuthGuard } from '@features/user-auth/guards';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
 @ApiTags('User')
-@Controller({ path: 'user', version: VERSIONING_API.v1 })
+@Controller({ path: SYSTEM_RESOURCE.user, version: VERSIONING_API.v1 })
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(UserAuthGuard)
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  me(@CurrentUser() user) {
+    return user;
+  }
 }
